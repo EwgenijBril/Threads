96389178a880929511d7d836d7bff209a90a5e4a
Evgeny <bril424@mail.ru>
2023-09-23 17:39:45 +0200

    components, validations, constants added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,8 +3,6 @@ import { ClerkProvider } from "@clerk/nextjs"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
